c05248bba408a23c22d28a1d567c148effd6fe83
Charles S Ferreira <ferreiracsf@gmail.com>
2025-11-28 12:34:55 -0300

    Tela de cadastro e login finalizados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,53 @@ import styles from './page.module.scss';
 import logoImg from '../../public/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { api } from '@/services/api';
+import { redirect } from 'next/navigation';
+import {cookies} from 'next/headers';
 
 export default function Page() {
+
+  async function handleLogin(formData: FormData) {
+    'use server';
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (!email || !password) {
+      return;
+    }
+
+    try {
+      const response =  await api.post('/session', {
+        email,
+        password
+      });
+
+      // console.log('Login successful:', response.data);
+
+      if (!response.data.token) {
+        return;
+      }
+
+      // Set cookie
+      const expressTime = 60 * 60 * 24 * 30 * 1000; // 30 days
+      const cookieStore = await cookies();
+      cookieStore.set('session', response.data.token, {
+        path: '/',
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: expressTime,
+      });
+
+    } catch (error) {
+      console.error('Error during login:', error);
+      return;
+    }
+
+
+    redirect('/dashboard');
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
@@ -11,7 +56,7 @@ export default function Page() {
         <Image src={logoImg} alt="Logo" />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type="email"
               name="email"
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,36 @@ import styles from '../page.module.scss';
 import logoImg from '../../../public/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { api } from '@/services/api';
+import { redirect } from 'next/navigation';
 
 export default function Signup() {
+
+  async function handleRegister(formData: FormData) {
+    'use server';
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (!name || !email || !password) {
+      return;
+    }
+
+    try {
+      await api.post('/users', {
+        name,
+        email,
+        password
+      });
+
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
+
+    redirect('/');
+    
+  }
+
     return (
         <>
             <div className={styles.containerCenter}>
@@ -13,7 +41,7 @@ export default function Signup() {
                 <section className={styles.login}>
                     <h1>Criando uma conta</h1>
 
-                    <form>
+                    <form action={handleRegister}>
                         <input
                             type="text"
                             name="name"
